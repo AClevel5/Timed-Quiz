1ee0f987a1929bc920cb508641499cc10a9a63d1
Alex Cleveland <Alexcleveland5@gmail.com>
2022-07-08 21:34:26 -0600

    Sudo Code, Timer, html structure

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,11 +4,38 @@
 
 //Question/Answer Object
 
+let question = document.createElement("p")
+
+const questions = {
+    Question1:"Ask question here",
+    Answer1:["strings","of","answers here"]
+}
+
 //Hide start button/ Start button start time
 
 //clear page div.welcome cleared - html div welcome and button element
 
 // countdownloop cant go below 0
+
+function countdown() {
+    var timeLeft = 75;
+  
+    // TODO: Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+    var timeInterval = setInterval(function () {
+     
+      if(timeLeft === 0) {
+        timeLeft--;
+         //timerEl.textContent = timeLeft + " seconds";
+         //countDown.appendChild(timer);
+      }
+      else{
+        clearInterval(timeInterval)
+      }
+      
+    }, 1000);
+      
+  }
+
 // question 
 
         //if user picks correctly show next question
